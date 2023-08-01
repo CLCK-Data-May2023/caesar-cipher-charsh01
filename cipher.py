a654a86cc44866cbb8f21f95a770531640ce1b80
charsh01 <johndoe@example.com>
2023-08-01 19:34:50 -0400

    updated. cipher now able to handle and encrypt capital letters.

--- cipher.py
+++ cipher.py
@@ -1,5 +1,6 @@
 # Alphabet list, to compare values and index locations.
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+alphabet = (("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"),
+            ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"))
 
 # Get sentence string and shift value from user.
 sentence_to_encrypt = input("Please enter a message to encrypt: ")
@@ -17,19 +18,26 @@ for word in words_to_encrypt:
     encrypted_word = []
     for letter in word:
         # Start loop with encryption status = false. Prevents alphabet characters from getting overwritten
-        # by non-alphabet character if conditions.
+        # by non-alphabet / capitalization character handling conditions below.
         letter_encrypted = False
-        for letter_location in alphabet:
+        # Tracks case of alphabet.
+        case = 0
+        # Checks if letter capitalized.
+        if letter.isupper() == True:
+            # Change to capitalized tuple in alphabet list.
+            case = 1
+        for letter_location in alphabet[case]:
+            # When letter in user input sentence matches the current letter in alphabet loop.
             if letter == letter_location:
-                # Location of input character in alphabet list.
-                new_letter_location = alphabet.index(letter_location)
-                # Shifts list location by user determined amount for cipher.
+                # Geet location of input character in alphabet tuple.
+                new_letter_location = alphabet[case].index(letter_location)
+                # Shifts tuple location by user determined amount for cipher.
                 new_letter_location += encryption_offset
-                # Loops end-of-alphabet characters to beginning.
+                # Loops end-of-alphabet characters to beginning if needed.
                 if new_letter_location > 25:
                     new_letter_location -= 26
-                # Replaces letter in word. Important to maintain list in list for maintaining sentence structure.
-                new_letter = letter.replace(letter, alphabet[new_letter_location])
+                # Replaces letter in word. Important to maintain sentence structure.
+                new_letter = letter.replace(letter, alphabet[case][new_letter_location])
                 letter_encrypted = True
             # Preserves non-alphabet characters.
             elif letter_location == "z" and letter_encrypted == False:
@@ -39,8 +47,8 @@ for word in words_to_encrypt:
     # Builds new encrypted sentences from the encrypted words.
     encrypted_sentence.append(encrypted_word)
 
-#Loop through list. Loops through letters, adding each to new print sentence. Loop through words, adding space
-# to end of words.
+#Loop through new sentence list. First through letters, adding each to new print sentence. Next through words,
+# adding space at end of words.
 print_sentence = ""
 for word in encrypted_sentence:
     for letter in word:
